8784cd060003e0c716df1c079c7cb80c3828872d
monrubi <wunderber8@hotmail.com>
2020-10-25 22:39:16 -0600

    got all info feed

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -8,7 +8,6 @@ export default class Pokemon extends Component {
         pokeImg:'',
         types: [],
         hab: [],
-        desc: '',
         stats:{
             hp:'',
             attack:'',
@@ -28,15 +27,68 @@ export default class Pokemon extends Component {
         const pokeRes = await Axio.get(infoURL);
         const name = pokeRes.data.name;
         const pokeImg = pokeRes.data.sprites.front_default;
+
+
+
+        let {hp, attack, defense, specialA, specialD, speed}='';
+        pokeRes.data.stats.map(stat => {
+            switch(stat.stat.name){
+                case 'hp':
+                    hp = stat['base_stat'];
+                    break;
+                case 'attack':
+                    attack = stat['base_stat'];
+                    break;
+                case 'defense':
+                    defense = stat['base_stat'];
+                    break;
+                case 'special-attack':
+                    specialA = stat['base_stat'];
+                    break;
+                case 'special-defense':
+                    specialD = stat['base_stat'];
+                    break;
+                case 'speed':
+                    speed = stat['base_stat'];
+                    break;
+            }
+        });
+
+        const height = pokeRes.data.height * 0.1;
+        const weight = pokeRes.data.weight * 0.1;
+        const types = pokeRes.data.types.map(type => type.type.name);
+        const hab = pokeRes.data.abilities.map(ability => ability.ability.name);
+
         this.setState({
-            name
+            pokeIdx,
+            pokeImg,
+            name,
+            types,
+            weight,
+            height,
+            stats:{
+                hp,
+                attack,
+                defense,
+                speed,
+                specialA,
+                specialD
+            },
+            hab
         });
     }
 
     render() {
         return (
-            <div>
-
+            <div className="col">
+                <div className="card-header">
+                    <div className="row">
+                        <div className="col-5">
+                            {this.state.pokeIdx}
+                        </div>
+                    </div>
+                </div>
+                
             </div>
         )
     }
